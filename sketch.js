d01dad9669a278d53bdc0b2063abbbd5c18c933a
tejaswanama111 <71207920+tejaswanama111@users.noreply.github.com>
2020-10-27 03:44:05 -0700

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,55 @@
+var  dog, dogImg, happyDog, database, foodS, foodStock;
+
+function preload()
+{
+  dogImg = loadImage("images/dog.png");
+  happyDog = loadImage("images/dog1.png");
+}
+
+function setup() {
+  createCanvas(500, 500);
+
+  dog = createSprite(250,250);
+  dog.addImage(dogImg);
+  dog.scale= 0.3;
+
+  database = firebase.database();
+  
+  foodStock = database.ref('Food');
+  foodStock.on("value", readStock);
+  
+}
+
+
+function draw() {  
+  background(46,139,87);
+
+  if(keyWentDown(UP_ARROW)){
+    writeStock(foodS);
+    dog.addImage(happyDog);
+  }
+
+  drawSprites();
+
+  textSize(17);
+  fill("white");
+  text("Food Remaining : " + foodStock, 10,50);
+}
+
+function readStock(data){
+  foodS = data.val();
+}
+
+function writeStock(x){
+
+  if(x<=0){
+    x = 0;
+  }
+  else{
+    x = x-1;
+  }
+
+  database.ref('/').update({
+    Food:x
+  })
+}
